a6cc6d0639aa76f7593f12728484bbbc9eb1c8ab
zhengp0 <zhengp@uw.edu>
2020-11-12 10:59:33 -0800

    add SplineSpecs class

--- src/regm/utils.py
+++ src/regm/utils.py
@@ -2,7 +2,9 @@
 Utility classes and functions
 """
 from typing import Any
+from dataclasses import dataclass
 import numpy as np
+from xspline import XSpline
 
 
 def default_vec_factory(vec: Any, size: int, default_value: float,
@@ -20,3 +22,33 @@ def default_vec_factory(vec: Any, size: int, default_value: float,
 
 def check_size(vec: Any, size: int, vec_name: str = 'vec'):
     assert len(vec) == size, f"{vec_name} must length {size}."
+
+
+@dataclass
+class SplineSpecs:
+    knots: np.ndarray
+    degree: int = 3
+    l_linear: bool = False
+    r_linear: bool = False
+    knots_type: str = "abs"
+
+    def __post_init__(self):
+        assert self.knots_type in ["abs", "rel_domain", "rel_freq"], \
+            "Knots type must be one of 'abs', 'rel_domain' or 'rel_freq'."
+
+    def create_spline(self, vec: np.ndarray = None) -> XSpline:
+        if self.knots_type == "abs":
+            knots = self.knots
+        else:
+            assert vec is not None, \
+                "Using relative knots, must provide a vector to finalize knots."
+            if self.knots_type == "rel_domain":
+                lb = np.min(vec)
+                ub = np.max(vec)
+                knots = lb + self.knots*(ub - lb)
+            else:
+                knots = np.quantile(vec, self.knots)
+
+        return XSpline(knots, self.degree,
+                       l_linear=self.l_linear,
+                       r_linear=self.r_linear)
