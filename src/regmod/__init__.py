c9bac54bdd29b4e662d42426e5e4387411584f7a
zhengp0 <zhengp@uw.edu>
2020-11-13 14:44:04 -0800

    add parameter module

--- src/regmod/__init__.py
+++ src/regmod/__init__.py
@@ -1,3 +1,4 @@
 from .data import Data
 from .prior import GaussianPrior, UniformPrior, SplineGaussianPrior, SplineUniformPrior
 from .variable import Variable, SplineVariable
+from .parameter import Parameter
---
+++ src/regmod/parameter.py
@@ -0,0 +1,61 @@
+"""
+Parameter module
+"""
+from typing import List, Callable
+from dataclasses import dataclass, field
+import numpy as np
+from scipy.linalg import block_diag
+from .data import Data
+from .variable import Variable, SplineVariable
+
+
+@dataclass
+class Parameter:
+    name: str
+    variables: List[Variable] = field(repr=False)
+    inv_link: Callable = field(repr=False)
+
+    @property
+    def size(self) -> int:
+        return sum([var.size for var in self.variables])
+
+    def check_data(self, data: Data):
+        for var in self.variables:
+            var.check_data(data)
+
+    def get_mat(self, data: Data) -> np.ndarray:
+        return np.hstack([var.get_mat(data) for var in self.variables])
+
+    def get_uvec(self) -> np.ndarray:
+        return np.hstack([var.get_uvec() for var in self.variables])
+
+    def get_gvec(self) -> np.ndarray:
+        return np.hstack([var.get_gvec() for var in self.variables])
+
+    def get_spline_uvec(self) -> np.ndarray:
+        uvec = np.hstack([
+            var.get_spline_uvec() if isinstance(var, SplineVariable) else np.empty((2, 0))
+            for var in self.variables
+        ])
+        return uvec
+
+    def get_spline_gvec(self) -> np.ndarray:
+        gvec = np.hstack([
+            var.get_spline_gvec() if isinstance(var, SplineVariable) else np.empty((2, 0))
+            for var in self.variables
+        ])
+        return gvec
+
+    def get_spline_umat(self) -> np.ndarray:
+        umat = block_diag(*[
+            var.get_spline_umat() if isinstance(var, SplineVariable) else np.empty((0, 1))
+            for var in self.variables
+        ])
+        return umat
+
+    def get_spline_gmat(self) -> np.ndarray:
+        gmat = block_diag(*[
+            var.get_spline_gmat() if isinstance(var, SplineVariable) else np.empty((0, 1))
+            for var in self.variables
+        ])
+        return gmat
