e0462b1afa5d6f16caa86fc25e2024203bb65f8a
Peng Zheng <zhengp@uw.edu>
2020-11-15 18:07:16 -0800

    Model: add Binomial model (#13)
    * add the binomial model
    
    * add tests for binomial model

--- src/regmod/model.py
+++ src/regmod/model.py
@@ -152,7 +152,7 @@ class PoissonModel(Model):
                         inv_link=inv_link,
                         use_offset=use_offset)
         assert all(data.obs >= 0), \
-            "Poisson model require observations to be non-negagive."
+            "Poisson model requires observations to be non-negagive."
         super().__init__(data, [lam])
 
     def nll(self, params: List[np.ndarray]) -> np.ndarray:
@@ -166,3 +166,38 @@ class PoissonModel(Model):
 
     def __repr__(self) -> str:
         return f"PoissonModel(num_obs={self.data.num_obs}, num_params={self.num_params}, size={self.size})"
+
+
+class BinomialModel(Model):
+    def __init__(self, data: Data, variables: List[Variable],
+                 inv_link: Union[str, SmoothFunction] = "expit",
+                 use_offset: bool = False):
+        p = Parameter(name="p",
+                      variables=variables,
+                      inv_link=inv_link,
+                      use_offset=use_offset)
+        assert np.all(data.obs >= 0), \
+            "Binomial model requires observations to be non-negative."
+        assert len(data.col_obs) == 2, \
+            "Binomial model need 2 columns of observations, one for number of events, one for sample size."
+        self.obs_1s = data.get_cols(data.col_obs[0])
+        self.obs_sample_sizes = data.get_cols(data.col_obs[1])
+        assert all(self.obs_1s <= self.obs_sample_sizes), \
+            "Binomial model requires number of events less or equal than sample size."
+        self.obs_0s = self.obs_sample_sizes - self.obs_1s
+        super().__init__(data, [p])
+
+    def nll(self, params: List[np.ndarray]) -> np.ndarray:
+        return -self.data.weights*(self.obs_1s*np.log(params[0]) +
+                                   self.obs_0s*np.log(1.0 - params[0]))
+
+    def dnll(self, params: List[np.ndarray]) -> List[np.ndarray]:
+        return [-self.data.weights*(self.obs_1s/params[0] -
+                                    self.obs_0s/(1.0 - params[0]))]
+
+    def d2nll(self, params: List[np.ndarray]) -> List[List[np.ndarray]]:
+        return [[np.diag(self.data.weights*(self.obs_1s/params[0]**2 +
+                                            self.obs_0s/(1.0 - params[0])**2))]]
+
+    def __repr__(self) -> str:
+        return f"BinomialModel(num_obs={self.data.num_obs}, num_params={self.num_params}, size={self.size})"
