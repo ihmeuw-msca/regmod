af2aa7525232d56e64ab4ba0cb48815f93ba998b
zhengp0 <zhengp@uw.edu>
2021-01-21 20:28:56 -0800

    add trimming optimizer decorator.

--- src/regmod/optimizer.py
+++ src/regmod/optimizer.py
@@ -1,14 +1,17 @@
 """
 Optimizer module
 """
+from typing import Callable, Dict
 import numpy as np
+from numpy import ndarray
 from scipy.optimize import LinearConstraint, minimize
 
 from regmod.models import Model
 
 
-def scipy_optimize(model: Model, x0: np.ndarray = None,
-                   options: dict = None) -> dict:
+def scipy_optimize(model: Model,
+                   x0: ndarray = None,
+                   options: Dict = None) -> Dict[str, ndarray]:
 
     x0 = np.zeros(model.size) if x0 is None else x0
     bounds = model.uvec.T
@@ -29,3 +32,28 @@ def scipy_optimize(model: Model, x0: np.ndarray = None,
     coefs = result.x
     vcov = np.linalg.inv(model.hessian(coefs))
     return {"coefs": coefs, "vcov": vcov}
+
+
+def trimming(optimize: Callable) -> Callable:
+    def optimize_with_trimming(model: Model,
+                               x0: ndarray = None,
+                               options: Dict = None,
+                               trim_steps: int = 3,
+                               inlier_pct: float = 0.95) -> Dict[str, ndarray]:
+        if trim_steps < 2:
+            raise ValueError("At least two trimming steps.")
+        if inlier_pct < 0.0 or inlier_pct > 1.0:
+            raise ValueError("inlier_pct has to be between 0 and 1.")
+        result = optimize(model, x0, options)
+        if inlier_pct < 1.0:
+            bounds = (0.5 - 0.5*inlier_pct, 0.5 + 0.5*inlier_pct)
+            index = model.detect_outliers(result["coefs"], bounds)
+            if index.sum() > 0:
+                for mask in np.linspace(1.0, 0.0, trim_steps)[1:]:
+                    weights = np.ones(model.data.num_obs)
+                    weights[index] = mask
+                    model.data.trim_weights = weights
+                    result = optimize(model, result["coefs"], options)
+                    index = model.detect_outliers(result["coefs"], bounds)
+        return result
+    return optimize_with_trimming
