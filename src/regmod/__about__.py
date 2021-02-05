af5bbd6c518fad2e53ad2514788afd80383891d5
zhengp0 <zhengp@uw.edu>
2021-02-05 10:26:34 -0800

    Advance to version v0.0.4

--- src/regmod/__about__.py
+++ src/regmod/__about__.py
@@ -7,7 +7,7 @@ __title__ = "regmod"
 __summary__ = "regmod: General regression models."
 __uri__ = "https://github.com/zhengp0/regmod"
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 
 __author__ = "Peng Zheng"
 __email__ = "zhengp@uw.edu"
