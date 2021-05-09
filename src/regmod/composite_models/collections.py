5d74dbb695ba509bc82bd966fcf87842f33286f8
zhengp0 <zhengp@uw.edu>
2021-05-09 09:35:51 -0700

    switch NamedList to inherit OrderedDict

--- src/regmod/composite_models/collections.py
+++ src/regmod/composite_models/collections.py
@@ -2,52 +2,29 @@
 Customize Collections Class
 """
 from collections import OrderedDict
-from typing import Any, List, Iterable, Iterator, Union
+from itertools import islice
+from typing import Any, Iterator, Union
 
 
-class NamedList:
+class NamedList(OrderedDict):
     """
     List with names, a wrapper around OrderedDict
     """
 
-    def __init__(self, keys: Iterable[str] = (), values: Iterable[Any] = ()):
-        self.ordered_dict = OrderedDict(zip(map(str, keys), values))
-
-    @property
-    def keys(self) -> List[str]:
-        return list(self.ordered_dict.keys())
-
-    @property
-    def values(self) -> List[str]:
-        return list(self.ordered_dict.values())
-
     def _as_key(self, key: Union[int, str]) -> str:
-        return self.keys[key] if isinstance(key, int) else str(key)
-
-    def append(self, key: str, value: Any):
-        self.ordered_dict.update({str(key): value})
-
-    def extend(self, keys: Iterable[str], values: Iterable[Any]):
-        for key, value in zip(keys, values):
-            self.append(key, value)
+        if isinstance(key, int):
+            index = key if key >= 0 else len(self) + key
+            key = next(islice(self.keys(), index, None))
+        return str(key)
 
     def pop(self, key: Union[int, str] = -1) -> Any:
-        return self.ordered_dict.pop(self._as_key(key))
-
-    def __contains__(self, key: str) -> bool:
-        return key in self.keys
+        return super().pop(self._as_key(key))
 
     def __iter__(self) -> Iterator:
-        return iter(self.values)
+        return iter(self.values())
 
     def __getitem__(self, key: Union[int, str]) -> Any:
-        return self.ordered_dict[self._as_key(key)]
+        return super().__getitem__(self._as_key(key))
 
     def __setitem__(self, key: Union[int, str], value: Any):
-        self.ordered_dict[self._as_key(key)] = value
-
-    def __len__(self) -> int:
-        return len(self.ordered_dict)
-
-    def __repr__(self) -> str:
-        return f"{type(self).__name__}(keys={self.keys}, values={self.values})"
+        super().__setitem__(self._as_key(key), value)
--- tests/test_namedlist.py
+++ tests/test_namedlist.py
@@ -11,7 +11,7 @@ from regmod.composite_models.collections import NamedList
 
 @pytest.fixture
 def nlist():
-    return NamedList(["a", "b", "c"], [1, 2, 3])
+    return NamedList({"a": 1, "b": 2, "c": 3})
 
 
 def test_default_init():
@@ -19,38 +19,12 @@ def test_default_init():
     assert len(nlist) == 0
 
 
-def test_keys(nlist):
-    assert nlist.keys == ["a", "b", "c"]
-
-
-def test_values(nlist):
-    assert nlist.values == [1, 2, 3]
-
-
-@pytest.mark.parametrize("a", [(1, 4), ("1", 4)])
-def test_append(nlist, a):
-    nlist.append(*a)
-    assert nlist.keys[-1] == "1"
-    assert nlist.values[-1] == 4
-
-
-def test_extend(nlist):
-    nlist.extend(["d", "f"], [4, 5])
-    assert nlist.keys[-2:] == ["d", "f"]
-    assert nlist.values[-2:] == [4, 5]
-
-
 @pytest.mark.parametrize("key", [-1, 2, "c"])
 def test_pop(nlist, key):
     value = nlist.pop(key)
     assert value == 3
-    assert len(nlist.keys) == 2
-    assert len(nlist.values) == 2
-
-
-@pytest.mark.parametrize(("key", "result"), [("a", True), ("d", False)])
-def test_contains(nlist, key, result):
-    assert (key in nlist) == result
+    assert len(nlist.keys()) == 2
+    assert len(nlist.values()) == 2
 
 
 @pytest.mark.parametrize("key", [-1, 2, "c"])
@@ -69,4 +43,4 @@ def test_len(nlist):
 
 
 def test_iter(nlist):
-    assert nlist.values == list(i for i in nlist)
+    assert list(nlist.values()) == list(i for i in nlist)
