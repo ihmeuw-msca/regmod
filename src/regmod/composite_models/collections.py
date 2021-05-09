b3bb6a640672fe876b1d608562c42298fab5824c
zhengp0 <zhengp@uw.edu>
2021-05-09 11:09:10 -0700

    add ChainNamedList class

--- src/regmod/composite_models/collections.py
+++ src/regmod/composite_models/collections.py
@@ -2,8 +2,8 @@
 Customize Collections Class
 """
 from collections import OrderedDict
-from itertools import islice
-from typing import Any, Iterator, Union
+from itertools import islice, chain
+from typing import Any, Iterator, Union, Tuple
 
 
 class NamedList(OrderedDict):
@@ -15,7 +15,7 @@ class NamedList(OrderedDict):
         if isinstance(key, int):
             index = key if key >= 0 else len(self) + key
             key = next(islice(self.keys(), index, None))
-        return str(key)
+        return key
 
     def pop(self, key: Union[int, str] = -1) -> Any:
         return super().pop(self._as_key(key))
@@ -28,3 +28,61 @@ class NamedList(OrderedDict):
 
     def __setitem__(self, key: Union[int, str], value: Any):
         super().__setitem__(self._as_key(key), value)
+
+
+class ChainNamedList:
+    """
+    A unified view of a set of NamedList
+    """
+
+    def __init__(self, *args):
+        self.named_lists = [
+            arg if isinstance(arg, NamedList) else NamedList(arg)
+            for arg in args
+        ]
+
+    def keys(self) -> Iterator:
+        return chain.from_iterable(nlst.keys() for nlst in self.named_lists)
+
+    def values(self) -> Iterator:
+        return chain.from_iterable(nlst.values() for nlst in self.named_lists)
+
+    def _as_key(self,
+                key: Union[int, str],
+                raise_keyerror: bool = True) -> Tuple[int, str]:
+        if isinstance(key, int):
+            index = key if key >= 0 else len(self) + key
+            key = next(islice(self.keys(), index, None))
+        i = next(
+            (i for i, nlst in enumerate(self.named_lists) if key in nlst), -1
+        )
+        if raise_keyerror and i == -1:
+            raise KeyError(f"'{key}'")
+        return i, key
+
+    def pop(self, key: Union[int, str] = -1) -> Any:
+        i, key = self._as_key(key)
+        return self.named_lists[i].pop(key)
+
+    def __contains__(self, key: str) -> bool:
+        return any(key in nlst for nlst in self.named_lists)
+
+    def __iter__(self) -> Iterator:
+        return iter(self.values())
+
+    def __getitem__(self, key: Union[int, str]) -> Any:
+        i, key = self._as_key(key)
+        return self.named_lists[i][key]
+
+    def __setitem__(self, key: Union[int, str], value: Any):
+        i, key = self._as_key(key, raise_keyerror=False)
+        i = i if i >= 0 else len(self.named_lists) + i
+        self.named_lists[i][key] = value
+
+    def __len__(self) -> int:
+        return sum(len(nlst) for nlst in self.named_lists)
+
+    def __repr__(self) -> str:
+        head = type(self).__name__
+        contents = ", ".join(map(str, self.named_lists))
+        return f"{head}({contents})"
--- tests/test_namedlist.py
+++ tests/test_namedlist.py
@@ -1,9 +1,9 @@
 """
-Test NamedList
+Test NamedList and ChainNamedList
 """
 import pytest
 
-from regmod.composite_models.collections import NamedList
+from regmod.composite_models.collections import NamedList, ChainNamedList
 
 
 # pylint:disable=redefined-outer-name
@@ -12,6 +12,16 @@ from regmod.composite_models.collections import NamedList
 @pytest.fixture
 def nlist():
     return NamedList({"a": 1, "b": 2, "c": 3})
+
+
+@pytest.fixture
+def other_nlist():
+    return NamedList({"d": 4, "e": 5, "f": 6})
+
+
+@pytest.fixture
+def chain_nlist(nlist, other_nlist):
+    return ChainNamedList(nlist, other_nlist)
 
 
 def test_default_init():
@@ -44,3 +54,45 @@ def test_len(nlist):
 
 def test_iter(nlist):
     assert list(nlist.values()) == list(i for i in nlist)
+
+
+def test_chain(chain_nlist):
+    assert len(chain_nlist.named_lists) == 2
+
+
+def test_chain_keys(chain_nlist):
+    assert list(chain_nlist.keys()) == ["a", "b", "c", "d", "e", "f"]
+
+
+def test_chain_values(chain_nlist):
+    assert list(chain_nlist.values()) == [1, 2, 3, 4, 5, 6]
+
+
+def test_chain_pop(chain_nlist):
+    assert chain_nlist.pop() == 6
+    assert chain_nlist.pop("a") == 1
+    assert len(chain_nlist) == 4
+
+
+def test_chain_contains(chain_nlist):
+    assert "a" in chain_nlist
+    assert "d" in chain_nlist
+    assert "bakka" not in chain_nlist
+
+
+def test_chain_getitem(chain_nlist):
+    assert chain_nlist["a"] == 1
+    assert chain_nlist[0] == 1
+    assert chain_nlist[-1] == 6
+    assert chain_nlist[4] == 5
+
+
+def test_chain_setitem(chain_nlist):
+    chain_nlist["a"] = 100
+    chain_nlist[4] = 500
+    assert chain_nlist[0] == 100
+    assert chain_nlist["e"] == 500
+
+
+def test_chain_iter(chain_nlist):
+    assert list(chain_nlist) == list(chain_nlist.values())
