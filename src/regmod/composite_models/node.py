99e44a61c172626d9b932332f7b6ece25236474e
zhengp0 <zhengp@uw.edu>
2021-09-07 11:14:58 -0700

    Add docstring for node class

--- src/regmod/composite_models/node.py
+++ src/regmod/composite_models/node.py
@@ -1,15 +1,60 @@
 """
 Tree Node
 """
-from itertools import chain
 from functools import reduce
-from operator import truediv, attrgetter
-from typing import Any, Iterable, List, Union
+from itertools import chain
+from operator import attrgetter, truediv
+from typing import Any, Iterable, List, Optional, Union
 
 from regmod.composite_models.collections import ChainNamedList
 
 
 class Node:
+    """Node class contains the hierarchy information.
+
+    Parameters
+    ----------
+    name : str
+        The name of the node.
+
+    Attributes
+    ----------
+    name : str
+        The name of the node.
+    parent : Optional[Node]
+        The parent node. If `None`, the node is the root node.
+    children : ChainNamedList
+        The children nodes. There are two compartments, one for primary children
+        and one for secondary children.
+    isroot : bool
+    isleaf : bool
+    full_name : str
+    root : Node
+    leafs : List[Node]
+    branch : List[Node]
+    tree : List[Node]
+    level : int
+
+    Methods
+    -------
+    append(node, rank=0)
+        Append node to the current node.
+    extend(nodes, rank=0)
+        Append nodes to the current node.
+    merge(node)
+        Merge current node with other node.
+    pop(name=-1, rank=None)
+        Pop the children node by name of index.
+    detach()
+        Detach from the parent.
+    get_name(level)
+        Get the name up to the given level.
+    get_leafs(*ranks)
+        Get the leafs with given rank.
+    copy()
+        Copy the current node.
+    """
+
     def __init__(self, name: str):
         self.name = name
         self.parent = None
@@ -26,24 +71,29 @@ class Node:
 
     @property
     def isroot(self) -> bool:
+        """Indicator if the node is the root node."""
         return self.parent is None
 
     @property
     def isleaf(self) -> bool:
+        """Indicator if the node is the leaf node."""
         return len(self.children) == 0
 
     @property
     def full_name(self) -> str:
+        """Full name include all ancestors."""
         return self.get_name(0)
 
     @property
     def root(self) -> "Node":
+        """The root node of current node."""
         if self.isroot:
             return self
         return self.parent.root
 
     @property
     def leafs(self) -> List["Node"]:
+        """The leaf nodes of current node."""
         if self.isleaf:
             return [self]
         return list(chain.from_iterable(
@@ -52,6 +102,7 @@ class Node:
 
     @property
     def branch(self) -> List["Node"]:
+        """The branch of current node."""
         if self.isleaf:
             return [self]
         return [self] + list(chain.from_iterable(
@@ -60,15 +111,31 @@ class Node:
 
     @property
     def tree(self) -> List["Node"]:
+        """The entire tree of current node."""
         return self.root.branch
 
     @property
     def level(self) -> int:
+        """The level of current node, level of a root node is 0."""
         if self.isroot:
             return 0
         return self.parent.level + 1
 
     def append(self, node: Union[str, "Node"], rank: int = 0):
+        """Append node to the current node.
+
+        Parameters
+        ----------
+        node : Union[str, Node]
+            The node need to be appended.
+        rank : int, optional
+            Indicate which compartment to append to, by default 0.
+
+        Raises
+        ------
+        ValueError
+            Rasied when the node to be appended already have parent.
+        """
         node = self.as_node(node)
         if not node.isroot:
             raise ValueError(f"Cannot append {node}, "
@@ -80,17 +147,57 @@ class Node:
             self.children.named_lists[rank][node.name] = node
 
     def extend(self, nodes: Iterable[Union[str, "Node"]], rank: int = 0):
+        """Append nodes to the current node.
+
+        Parameters
+        ----------
+        nodes : Iterable[Union[str, Node]]
+            The node need to be appended.
+        rank : int, optional
+            Indicate which compartment to append to, by default 0.
+        """
         for node in nodes:
             self.append(node, rank=rank)
 
     def merge(self, node: Union[str, "Node"]):
+        """Merge current node with other node.
+
+        Parameters
+        ----------
+        node : Union[str, Node]
+            Node to be merged with.
+
+        Raises
+        ------
+        ValueError
+            Raised when the other nodes have a different name.
+        """
         if node.name != self.name:
             raise ValueError("Cannot merge with node with different name.")
         for rank, children in enumerate(node.children.named_lists):
             while len(children) > 0:
                 self.append(node.pop(rank=rank), rank=rank)
 
-    def pop(self, name: Union[int, str] = -1, rank: int = None) -> "Node":
+    def pop(self,
+            name: Union[int, str] = -1,
+            rank: Optional[int] = None) -> "Node":
+        """Pop the children node by name of index.
+
+        Parameters
+        ----------
+        name : Union[int, str], optional
+            The name or index of the children node. Default to -1 which is the
+            last added children node.
+        rank : Optional[int], optional
+            Indicate which compartment to append to, by default None. When it
+            is None, it will automatically find which compartment the name
+            belongs to and then pop.
+
+        Returns
+        -------
+        Node
+            Poped children node.
+        """
         children = self.children
         if rank is not None:
             children = self.children.named_lists[rank]
@@ -99,15 +206,36 @@ class Node:
         return node
 
     def detach(self):
+        """Detach from the parent.
+        """
         if not self.isroot:
             self.parent.pop(self.name)
 
     def get_name(self, level: int) -> str:
+        """Get the name up to the given level.
+
+        Parameters
+        ----------
+        level : int
+            Given level.
+
+        Returns
+        -------
+        str
+            Name up to the given level.
+        """
         if self.level <= level or self.isroot:
             return self.name
         return "/".join([self.parent.get_name(level), self.name])
 
     def get_leafs(self, *ranks) -> List["Node"]:
+        """Get the leafs with given rank.
+
+        Returns
+        -------
+        List[Node]
+            A list of leaf nodes.
+        """
         if len(ranks) == 0:
             ranks = range(len(self.children.named_lists))
         leafs = list(chain.from_iterable(
@@ -118,6 +246,13 @@ class Node:
         return leafs
 
     def copy(self) -> "Node":
+        """Copy the current node.
+
+        Returns
+        -------
+        Node
+            A copy of the current node.
+        """
         return self.__copy__()
 
     def __getitem__(self, name: str) -> "Node":
@@ -188,15 +323,51 @@ class Node:
 
     @classmethod
     def as_node(cls, obj: Any) -> "Node":
+        """Convert object into instance of Node.
+
+        Parameters
+        ----------
+        obj : Any
+            Object to be converted.
+
+        Returns
+        -------
+        Node
+            Converted node.
+        """
         if isinstance(obj, Node):
             return obj
         return cls(str(obj))
 
     @classmethod
     def from_names(cls, names: Iterable[str]) -> "Node":
+        """Create chain of nodes by their names.
+
+        Parameters
+        ----------
+        names : Iterable[str]
+            A list of names.
+
+        Returns
+        -------
+        Node
+            Root node of the chain.
+        """
         return reduce(truediv, map(cls.as_node, names)).root
 
     @classmethod
     def from_full_name(cls, full_name: str) -> "Node":
+        """Create chain of nodes by the full name of the leaf node.
+
+        Parameters
+        ----------
+        full_name : str
+            Full name of the leaf node.
+
+        Returns
+        -------
+        Node
+            Root node of the chain.
+        """
         names = full_name.split("/")
         return cls.from_names(names)
