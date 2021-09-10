d0597f8e89f9cb03a61a2ef3c4c1aee4aa1738a4
zhengp0 <zhengp@uw.edu>
2021-09-10 14:24:11 -0700

    Add the docstring to the tree model

--- src/regmod/composite_models/tree.py
+++ src/regmod/composite_models/tree.py
@@ -12,8 +12,12 @@ from regmod.composite_models.interface import NodeModel
 
 
 class TreeModel(CompositeModel):
-    """
-    Tree Model with hierarchy structure. This model is also called cascade.
+    """Tree Model with hierarchy structure. This model is also called Cascade.
+
+    Methods
+    -------
+    fit()
+        Overwrite the fit function in CompositeModel.
     """
 
     def _fit(self, model: NodeModel, **fit_options):
@@ -24,6 +28,13 @@ class TreeModel(CompositeModel):
             self._fit(sub_model, **fit_options)
 
     def fit(self, **fit_options):
+        """Overwrite the fit function in CompositeModel.
+
+        Raises
+        ------
+        ValueError
+            Raised when model have more than one computational tree.
+        """
         if len(self.children.named_lists[1]) != 1:
             raise ValueError(f"{type(self).__name__} must only have one "
                              "computational tree.")
@@ -31,6 +42,13 @@ class TreeModel(CompositeModel):
 
     @classmethod
     def get_simple_tree(cls, name: str, *args, **kwargs) -> "TreeModel":
+        """Get the simple TreeModel from given specification.
+
+        Returns
+        -------
+        TreeModel
+            A simple tree model.
+        """
         return cls(name, models=[get_simple_basetree(*args, **kwargs)])
 
 
@@ -39,6 +57,26 @@ def get_simple_basetree(df: DataFrame,
                         model_specs: Dict,
                         var_masks: Dict[str, float] = None,
                         lvl_masks: List[float] = None) -> BaseModel:
+    """Get a simple TreeModel from given specification.
+
+    Parameters
+    ----------
+    df : DataFrame
+        Given data frame
+    col_label : List[str]
+        The name of the label column
+    model_specs : Dict
+        Model specification including data and variables.
+    var_masks : Dict[str, float], optional
+        Prior masks for variables., by default None.
+    lvl_masks : List[float], optional
+        Prior masks for each level, by default None.
+
+    Returns
+    -------
+    BaseModel
+        Root node of the computational tree model.
+    """
     # check data before create model
     data = model_specs["data"]
     variables = model_specs["variables"]
