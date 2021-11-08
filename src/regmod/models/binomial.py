48616b72f348d60adfbe4b80beb5c73ca516f1af
zhengp0 <zhengp@uw.edu>
2021-11-08 12:32:30 -0800

    Simplify the binomial model observation

--- src/regmod/models/binomial.py
+++ src/regmod/models/binomial.py
@@ -16,28 +16,19 @@ class BinomialModel(Model):
     default_param_specs = {"p": {"inv_link": "expit"}}
 
     def __init__(self, data: Data, **kwargs):
-        if not np.all(data.obs >= 0):
-            raise ValueError("Binomial model requires observations to be non-negative.")
-        if len(data.col_obs) != 2:
-            raise ValueError("Binomial model need 2 columns of observations, "
-                             "one for number of events, one for sample size.")
-        if any(np.diff(data.get_cols(data.col_obs), axis=1) < 0):
-            raise ValueError("Binomial model requires number of events less or equal than sample size.")
-
-        self.obs_1s = data.get_cols(data.col_obs[0])
-        self.obs_0s = np.diff(data.get_cols(data.col_obs), axis=1).ravel()
-        self.obs_sample_sizes = data.get_cols(data.col_obs[1])
-
+        if not np.all((data.obs >= 0) & (data.obs <= 1)):
+            raise ValueError("Binomial model requires observations to be "
+                             "between zero and one.")
         super().__init__(data, **kwargs)
 
     def nll(self, params: List[ndarray]) -> ndarray:
-        return -(self.obs_1s*np.log(params[0]) + self.obs_0s*np.log(1.0 - params[0]))
+        return -(self.data.obs*np.log(params[0]) + (1 - self.data.obs)*np.log(1.0 - params[0]))
 
     def dnll(self, params: List[ndarray]) -> List[ndarray]:
-        return [-(self.obs_1s/params[0] - self.obs_0s/(1.0 - params[0]))]
+        return [-(self.data.obs/params[0] - (1 - self.data.obs)/(1.0 - params[0]))]
 
     def d2nll(self, params: List[ndarray]) -> List[List[ndarray]]:
-        return [[self.obs_1s/params[0]**2 + self.obs_0s/(1.0 - params[0])**2]]
+        return [[self.data.obs/params[0]**2 + (1 - self.data.obs)/(1.0 - params[0])**2]]
 
     def get_ui(self, params: List[ndarray], bounds: Tuple[float, float]) -> ndarray:
         p = params[0]
